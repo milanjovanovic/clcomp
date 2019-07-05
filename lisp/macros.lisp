9d829ba4121c83171408faf4c010649fba62ea18
Milan Jovanovic <milanj@gmail.com>
2019-07-05 21:29:01 +0200

    macros

--- lisp/macros.lisp
+++ lisp/macros.lisp
@@ -1,6 +1,7 @@
 (in-package :clcomp)
 
 (defparameter *macros* (make-hash-table))
+(defparameter *setf-expanders* (make-hash-table))
 
 (defun macro-dotimes (form)
   (let ((limit (gensym "LIMIT-"))
@@ -46,58 +47,117 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defstruct env)
+(defstruct env blocks)
 
-(defun %clcomp-macroexpand-let-bindings (bindings)
+(defun get-block-or-return-from-symbol (env block-symbol base-name)
+  (let* ((hash-key (concatenate 'string (symbol-name block-symbol) base-name))
+	 (sym (gethash hash-key (env-blocks env))))
+    (if sym
+	sym
+	(let ((sym (gensym base-name)))
+	  (setf (gethash hash-key (env-blocks env)) sym)
+	  sym))))
+
+(defun block-to-value-name (env block-name)
+  (get-block-or-return-from-symbol env block-name "BLOCK-LET-VAR-"))
+
+(defun block-to-label-name (env block-name)
+  (get-block-or-return-from-symbol env block-name "BLOCK-TAGBODY-"))
+
+(defun %clcomp-macroexpand-let-bindings (bindings env)
   (mapcar (lambda (b)
-	    (list (first b) (clcomp-macroexpand (second b))))
+	    (list (first b) (clcomp-macroexpand (second b) env)))
 	  bindings))
 
-(defun %clcomp-macroexpand-let (form)
-  (cons (list 'let (%clcomp-macroexpand-let-bindings (second form)))
-	(mapcar #'clcomp-macroexpand (cddr form))))
+(defun %clcomp-macroexpand-let (form env)
+  (cons (list 'let (%clcomp-macroexpand-let-bindings (second form) env))
+	(mapcar (lambda (f)
+		  (clcomp-macroexpand f env))
+		(cddr form))))
 
-;;; FIXME
-(defun %clcomp-macroexpand-block (form)
-  (cons (list 'block (second form))
-	(mapcar #'clcomp-macroexpand (cddr form))))
+(defun %clcomp-macroexpand-block (form env)
+  (let ((tagbody-symbol (block-to-label-name env (second form)))
+	(ret-value-symbol (block-to-value-name env (second form))))
+    (clcomp-macroexpand
+     (list 'let (list (list ret-value-symbol nil))
+	   (list 'progn
+		 (list 'tagbody
+		       (list 'setq ret-value-symbol
+			     (append (list 'progn) (nthcdr 2 form)))
+		       tagbody-symbol)
+		 ret-value-symbol))
+     env)))
 
-(defun %clcomp-macroexpand-return-from (form)
-  (list 'return-from * (second form) (clcomp-macroexpand (third form))))
+(defun %clcomp-macroexpand-return-from (form env)
+  (let ((tagbody-symbol (block-to-label-name env (second form)))
+	(ret-value-symbol (block-to-value-name env (second form))))
+    (clcomp-macroexpand
+     (list 'progn
+	   (list 'setq ret-value-symbol (third form))
+	   (list 'go tagbody-symbol))
+     env)))
 
-(defun %clcomp-macroexpand-fun-call (form)
-  (cons (first form)
-	(cons (second form)
-	      (mapcar #'clcomp-macroexpand (cddr form)))))
+(defun %clcomp-macroexpand-all (form env)
+  (mapcar (lambda (f) (clcomp-macroexpand f env))
+	  form))
 
-(defun %clcomp-macroexpand-progn (form)
+(defun %clcomp-macroexpand-progn (form env)
   (cons 'progn
-	(mapcar #'clcomp-macroexpand (cdr form))))
+	(mapcar (lambda (f)
+		       (clcomp-macroexpand f env))
+		(cdr form))))
 
-(defun %clcomp-macroexpand (macro-form)
+(defun %clcomp-macroexpand (macro-form env)
   (let* ((macro-fun (gethash (first macro-form) *macros*))
 	 (expanded (funcall macro-fun macro-form)))
     (if (consp expanded)
-	(mapcar #'clcomp-macroexpand expanded)
+	(mapcar (lambda (f)
+		  (clcomp-macroexpand f env)) expanded)
 	expanded)))
 
+;; FIXME, %rt-defun ?!?!
+(defun %clcomp-macroexpand-defun (form env)
+  (list 'progn (list 'eval-when '(:compile-toplevel) (list '%rt-defun (second form)))
+	(list '%defun (second form)
+	      (clcomp-macroexpand (list 'lambda (third form) (fourth form)) env))))
+
+(defun %clcomp-macroexpand-eval-when (form env)
+  (list 'eval-when (second form)
+	(clcomp-macroexpand (cons 'progn (cddr form)) env)))
+
+(defun %clcomp-macroexpand-lambda (form env)
+  (list 'lambda (second form)
+	(clcomp-macroexpand (cons 'progn (cddr form)) env)))
+
+;; FIXME, implement setf macro expanders
+(defun %clcomp-macroexpand-setf (form env)
+  (let ((where (second form))
+	(what (third form)))
+    (if (symbolp where)
+	(list 'setq where (clcomp-macroexpand what env))
+	(let* ((accessor (first where)))
+	  (cond
+	    ((eq accessor 'car)
+	     (list '%setf-car (clcomp-macroexpand (second where) env) (clcomp-macroexpand what env)))
+	    ((eq accessor 'cdr)
+	     (list '%setf-cdr (clcomp-macroexpand (second where) env) (clcomp-macroexpand what env))))))))
+
 (defun clcomp-macroexpand (form &optional env)
+  (unless env
+    (setf env (make-env :blocks (make-hash-table :test 'equalp))))
   (if (atom form)
       form
       (let ((first (first form)))
 	(let ((macro-fun (gethash first *macros*)))
 	  (if macro-fun
-	      (%clcomp-macroexpand form)
+	      (%clcomp-macroexpand form env)
 	      (case first
-		(let (%clcomp-macroexpand-let form))
-		(block (%clcomp-macroexpand-block form))
-		(return (%clcomp-macroexpand-return-from form))
-		(progn (%clcomp-macroexpand-progn form))
-		(defun form)
-		(lambda form)
-		(setf form)
-		(otherwise (%clcomp-macroexpand-fun-call form))))))))
-
-
-
-
+		(let (%clcomp-macroexpand-let form env))
+		(block (%clcomp-macroexpand-block form env))
+		(return-from (%clcomp-macroexpand-return-from form env))
+		(progn (%clcomp-macroexpand-progn form env))
+		(defun (%clcomp-macroexpand-defun form env))
+		(eval-when (%clcomp-macroexpand-eval-when form env))
+		(lambda (%clcomp-macroexpand-lambda form env))
+		(setf (%clcomp-macroexpand-setf form env))
+		(otherwise (%clcomp-macroexpand-all form env))))))))
