f4026cf7524f0ce97d47faffb7152a1a70c5e1bb
Milan Jovanovic <milanj@gmail.com>
2019-11-05 00:09:38 +0100

    rename built_test.sh script

--- runtime/build_runtime.sh
+++ runtime/build_runtime.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-gcc -shared -o runtime.so -fPIC entry.S utils.c lispo.c runtime.c
+rm -f test
+#gcc -arch x86_64 -pagezero_size 0x100000 -o test entry.S utils.c lispo.c runtime.c 
+#gcc -arch x86_64 -pagezero_size 0x2000000 -o runtime.exe entry.S utils.c lispo.c runtime.c -g
+gcc -arch x86_64 -pagezero_size 0x2000000 -o runtime.exe entry.S utils.c lispo.c runtime.c -g -dynamic -twolevel_namespace -bind_at_load -Wl,-no_pie
+
+nm -g -U -P -t d runtime.exe > runtime.nm
