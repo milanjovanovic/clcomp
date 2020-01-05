82673d83e282ccc304c4626e6a610b5e18450423
Milan Jovanovic <milanj@gmail.com>
2020-01-05 11:35:20 +0100

    fix tests

--- mix/compiler-tests.lisp
+++ mix/compiler-tests.lisp
@@ -29,6 +29,7 @@
 
 
 (define-compiler-test "simple-test-1" t (lambda ()
+					  (%initialize-env)
 					  (= 0 0)))
 
 (define-compiler-test "simple-test-2" nil (lambda ()
@@ -37,6 +38,7 @@
 
 ;;; CONS/LIST
 (define-compiler-test "cons-test-1" t (lambda ()
+					(%initialize-env)
 					(let ((l (cons 1 (cons 2 (cons 3 nil)))))
 					  (setf (car l) 10)
 					  (let ((c (car l)))
@@ -44,66 +46,92 @@
 
 (define-compiler-test "cons-test-1-inline" t (lambda ()
 					       (declare (inline cons car))
+					       (%initialize-env)
 					       (let ((l (cons 1 (cons 2 (cons 3 nil)))))
 						 (setf (car l) 10)
 						 (let ((c (car l)))
 						   (= c 10)))))
 
 (define-compiler-test "list-test-1" t (lambda ()
+					(%initialize-env)
 					(let ((l (list 1 2 3 4 5 6)))
 					  (= 6 (+ (car l) 5)))))
 
 (define-compiler-test "list-test-1-inline" t (lambda ()
 					       (declare (inline car))
+					       (%initialize-env)
 					       (let ((l (list 1 2 3 4 5 6)))
 						 (= 6 (+ (car l) 5)))))
 
 (define-compiler-test "list-test-2" t (lambda ()
+					(%initialize-env)
 					(let ((l (list 1 2 3 4 5 6)))
 					  (= 6 (list-length l)))))
 
 (define-compiler-test "list-test-3" t (lambda ()
+					(%initialize-env)
 					(let ((l (list 1 2 3 4 5 6)))
 					  (= 4 (list-length (cddr l))))))
 
 (define-compiler-test "list-test-4" nil (lambda ()
+					  (%initialize-env)
 					  (let ((l (list 1 2 3 4 5 6)))
 					    (= 4 (list-length l)))))
+
 (define-compiler-test "list-test-5" t (lambda ()
+					(%initialize-env)
 					(let ((l (list 1 2 3 4 5 6)))
 					  (and
 					   (= 6 (car (list-reverse l)))
 					   (= 6 (list-length (list-reverse l)))))))
 
 (define-compiler-test "list-test-6" t (lambda ()
+					(%initialize-env)
 					(= 3 (length (list 1 2 3)))))
 
 (define-compiler-test "list-append-1" t (lambda ()
+					  (%initialize-env)
 					  (= 6 (length (append (list 1 2)
 							       (list 3 4)
 							       (list 5 6))))))
 
 (define-compiler-test "list-test-7" t (lambda ()
+					(%initialize-env)
 					(= 3 (cddr (list* 1 2 3)))))
 
 
 ;;; ARRAY
 (define-compiler-test "array-test-1"t (lambda ()
-					(let ((a (make-array 3 (cons 1 (cons 2 (cons 3 nil))))))
+					(%initialize-env)
+					(let ((a (make-array 3 t nil  (cons 1 (cons 2 (cons 3 nil))))))
 					  (setf (aref a 1) 20)
 					  (and (= (aref a 0) 1)
 					       (= (aref a 1) 20)
 					       (= (aref a 2) 3)))))
 
 (define-compiler-test "array-test-2" nil (lambda ()
-					   (let ((a (make-array 3 (cons 1 (cons 2 (cons 3 nil))))))
+					   (%initialize-env)
+					   (let ((a (make-array 3 t nil (cons 1 (cons 2 (cons 3 nil))))))
 					     (setf (aref a 1) 20)
 					     (and (= (aref a 0) 1)
 						  (= (aref a 1) 20)
 						  (= (aref a 2) 300)))))
 
-;;; STRINGe
+
+(define-compiler-test "array-test-3" t (lambda ()
+					   (%initialize-env)
+					   (let ((a (make-array 3 'fixnum 0 nil))
+						 (atype (type-of a)))
+					     (and (= (aref a 0) 0)
+						  (= (aref a 1) 0)
+						  (= (aref a 2) 0)
+						  (and (eq 'simple-array (first atype))
+						       (eq 'fixnum (second atype))
+						       (= 3 (first (third atype))))))))
+
+;;; STRING
 (define-compiler-test "string-test-1" t (lambda ()
+					  (%initialize-env)
 					  (let* ((s1 "test string")
 						 (s2 (%copy-array s1))
 						 (s3 (string-upcase s2)))
@@ -111,6 +139,26 @@
 						 (char= (char s3 0) #\T)
 						 (char= (char s3 1) #\E)
 						 (char-equal (char s3 0) (char s2 0) (char s1 0))))))
+
+
+;;; MIX
+(define-compiler-test "mix-1" t (lambda ()
+				  (%initialize-env)
+				  (let ((symbol 'bla))
+				    (and (eq 'foo 'foo)
+					 (not (eq 'foo 'bar))
+					 (eq 'bla symbol)
+					 (eq 'fixnum (type-of 10))))))
+
+(define-compiler-test "mix-1" t (lambda ()
+				  (%initialize-env)
+				  (let ((symbol 'bla))
+				    (and (eq 'foo 'foo)
+					 (not (eq 'foo 'bar))
+					 (eq 'bla symbol)
+					 (eq 'fixnum (type-of 10))))))
+
+
 
 
 
