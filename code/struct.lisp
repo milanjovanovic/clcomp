cb1b96a98d0e198319d3db874e059c9113c846fd
Milan Jovanovic <milanj@gmail.com>
2020-11-24 14:35:26 +0100

    missing struct files

---
+++ code/struct.lisp
@@ -0,0 +1,27 @@
+(defun %allocate-struct (slot-count type)
+  (declare (inline %allocate-struct))
+  (%allocate-struct slot-count type))
+
+(defun %struct-type (struct)
+  (declare (inline %struct-type))
+  (%struct-type struct))
+
+(defun %structp (obj)
+  (declare (inline %structp))
+  (%structp obj))
+
+(defun %get-struct-slot (struct slot-index)
+  (declare (inline %get-struct-slot))
+  (%get-struct-slot struct slot-index))
+
+(defun %set-struct-slot (struct slot-index value)
+  (declare (inline %set-struct-slot))
+  (%set-struct-slot struct slot-index value))
+
+(defun %make-struct (slot-count type slot-values)
+  (let ((struct (%allocate-struct slot-count type)))
+    (let ((index 0))
+      (dolist (slot slot-values)
+	(%set-struct-slot struct index slot)
+	(setf index (+ 1 index))))
+    struct))
